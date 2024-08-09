3dbaf18f3660e20335dadf21aa89b75b2868f4b9
Xinyang Wang <136667147+mrkingseasheep@users.noreply.github.com>
2024-08-09 00:46:18 -0400

    Attempting to add prev cmd func to + and ?

--- src/Server.cpp
+++ src/Server.cpp
@@ -4,10 +4,12 @@
 #include <string>
 #include <vector>
 
-bool has_pattern(const char* input, const char* pattern);
+bool has_pattern(const char* input, const char* pattern,
+                 std::string& prevPattern);
 bool match_all(const char* input, const char* pattern);
 
-void print_word(const char* input, const char* pattern) {
+void print_word(const char* input, const char* pattern,
+                std::string& prevPattern) {
     do {
         std::cout << *input;
     } while (*input++ != '\0');
@@ -16,56 +18,87 @@ void print_word(const char* input, const char* pattern) {
         std::cout << *pattern;
     } while (*pattern++ != '\0');
     std::cout << std::endl;
+    std::cout << "=========================================> PREV CMD: "
+              << prevPattern << std::endl;
 }
 
-bool has_espace_char(const char* input, const char* pattern) {
+bool has_espace_char(const char* input, const char* pattern,
+                     std::string& prevPattern) {
     if (*(pattern + 1) == 'd') {
         if (isdigit(*input)) {
-            return has_pattern(input + 1, pattern + 2);
+            prevPattern = "\\d";
+            return has_pattern(input + 1, pattern + 2, prevPattern);
         }
     }
 
     if (*(pattern + 1) == 'w') {
         if (isalnum(*input)) {
-            return has_pattern(input + 1, pattern + 2);
+            prevPattern = "\\w";
+            return has_pattern(input + 1, pattern + 2, prevPattern);
         }
     }
 
     if (*(pattern + 1) == '\\') {
         if (*pattern == *input) {
-            return has_pattern(input + 2, pattern + 2);
+            prevPattern = "\\\\";
+            return has_pattern(input + 2, pattern + 2, prevPattern);
         }
     }
 
     return false;
 }
 
-bool has_char_group(const char* input, const char* pattern) {
+bool has_char_group(const char* input, const char* pattern,
+                    std::string& prevPattern) {
     // find all char in group
     std::vector<char> charGroup;
-    bool negativeGroup = (*(pattern + 1) == '^');
-    int idx = negativeGroup + 1;
+    bool wantedGroup = !(*(pattern + 1) == '^');
+    int idx = !wantedGroup + 1;
+    bool containsGroup = false;
+    prevPattern = "[";
+    if (!wantedGroup) {
+        prevPattern += "^";
+    }
 
     while (*(pattern + idx) != ']') {
         charGroup.push_back(*(pattern + idx));
+        prevPattern += *(pattern + idx);
         ++idx;
     }
-
-    std::cout << negativeGroup << std::endl;
-    for (char curChar : charGroup) {
-        std::cout << curChar << std::endl;
-    }
+    prevPattern += "]";
 
     for (auto i = charGroup.cbegin(); i != charGroup.cend(); ++i) {
+        // matching special escape codes
         if (*i == '\\') {
+            if (*(i + 1) == '\\' && *input == '\\') {
+                containsGroup = true;
+            } else if (*(i + 1) == 'w' && isalnum(*input)) {
+                containsGroup = true;
+            } else if (*(i + 1) == 'd' && isdigit(*input)) {
+                containsGroup = true;
+            } // other cases are possible, I'm too lazy to implement them
         }
+
+        // matching normal chars
+        if (*i == *input) {
+            containsGroup = true;
+        }
+
+        if (containsGroup && wantedGroup) {
+            return has_pattern(input + 1, pattern + idx + 1, prevPattern);
+        }
+    }
+
+    if (!containsGroup && !wantedGroup) {
+        return has_pattern(input + 1, pattern + idx + 1, prevPattern);
     }
 
     return false;
 }
 
-bool has_pattern(const char* input, const char* pattern) {
-    print_word(input, pattern);
+bool has_pattern(const char* input, const char* pattern,
+                 std::string& prevPattern) {
+    print_word(input, pattern, prevPattern);
 
     // if pattern is empty, rest of input doesn't matter
     if (*pattern == '\0') {
@@ -73,16 +106,17 @@ bool has_pattern(const char* input, const char* pattern) {
     }
 
     if (*pattern == '\\') {
-        return has_espace_char(input, pattern);
+        return has_espace_char(input, pattern, prevPattern);
     }
 
     if (*pattern == '[') {
-        return has_char_group(input, pattern);
+        return has_char_group(input, pattern, prevPattern);
     }
 
     if (*pattern == '.') {
         if (*input != '\0') {
-            return has_pattern(input + 1, pattern + 1);
+            prevPattern = ".";
+            return has_pattern(input + 1, pattern + 1, prevPattern);
         }
         return false;
     }
@@ -93,7 +127,8 @@ bool has_pattern(const char* input, const char* pattern) {
 
     // if both chars are the same
     if (*input == *pattern) {
-        return has_pattern(input + 1, pattern + 1);
+        prevPattern = *pattern;
+        return has_pattern(input + 1, pattern + 1, prevPattern);
     }
 
     // in case nothing happens
@@ -101,6 +136,8 @@ bool has_pattern(const char* input, const char* pattern) {
 }
 
 bool root_match(const char* input, const char* pattern) {
+    std::string prevPattern; // litterally only for the + operator
+
     // get rid of edge case
     if (*pattern == '^') {
         return *input == *(pattern + 1);
@@ -108,7 +145,7 @@ bool root_match(const char* input, const char* pattern) {
 
     do {
         std::cout << "-------------------------------------\n";
-        if (has_pattern(input, pattern)) {
+        if (has_pattern(input, pattern, prevPattern)) {
             return true;
         }
     } while (*input++ != '\0');
